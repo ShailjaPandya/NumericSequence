8408e97fa77125ecdc12bbc1e752ad0ae3b78aac
ShailjaPandya <shailja2479@yahoo.com>
2016-06-15 06:44:55 +0000

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NumericSequence.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+    public ViewResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ViewResult fibonacci(NumericSequence.Models.InputModel model)
+    {
+        if (model.Number > 1)
+        {
+            Int64 a = 0;
+            Int64 b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (Int64 i = 0; i < model.Number; i++)
+            {
+                Int64 temp = a;
+                a = b;
+                string seriesValue = isDivRem(temp + b);
+                //Assumption: as not mentioned in story whether to add value whcih is Multiple of 3 or 5 or both 
+                //but displaying C,E, or Z
+                b = temp + b;
+                if (seriesValue == string.Empty)
+                    ViewData["sequence"] = ViewData["sequence"]  + "<p>fibonacci series " + b.ToString() + "</p>";
+                //Response.Write("<p>fibonacci series " + b.ToString() + "</p>");
+                else
+                    ViewData["sequence"] = ViewData["sequence"]  + "<p>fibonacci series " + seriesValue + " </p>";
+                    //Response.Write("<p>fibonacci series " + seriesValue + " </p>");
+            }
+            return View("Index");
+        }
+        return View("Index");
+    }
+
+
+/// <summary>
+/// Find Remainder of 3 or 5 and both.
+/// </summary>
+/// <param name="dividend"></param>
+/// <returns>Character to Display</returns>
+    private string isDivRem(long dividend)
+    {
+        string display = string.Empty;
+        long remainderOfThree;
+        long remainderOfFive;
+        
+        remainderOfThree = dividend % 3;
+        remainderOfFive = dividend % 5;
+        
+        if (remainderOfThree == 0){
+            display = "C";
+        }
+
+        if (remainderOfFive == 0){
+            if (display != string.Empty)
+                display = ",E";
+            else
+                display = "E";
+        }
+
+        if (display.Contains(","))
+            display = "Z";
+       
+        return display;
+    }
+
+}
+   
+}
---
+++ Models/InputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NumericSequence.Models
+{
+    public class InputModel
+    {
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Not a number")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Count must be a natural number")]
+        [Required(ErrorMessage = "{0} You forgot to enter a number.")]
+        [Display(Name = "Number")]
+        public int Number { get; set; }
+    }
+}
